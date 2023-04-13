b74e7c9e2c5a7035f0d91b475a97ac438ba25ede
Bilguun <soproatlol@gmail.com>
2023-04-13 22:28:45 +0200

    Folder cleanup & added Indie only tagDistribution

---
+++ dataScripts/initSteamGameDatabase.ts
@@ -0,0 +1,105 @@
+import { getServices } from "../services/getServices";
+import getInfra from "../infra/getInfrastructure";
+import { BaseGame, FailedSteamGame } from "../infra/sqlite-database";
+import { addDays, isBefore } from "date-fns";
+import { SteamApp } from "../infra/steam-api";
+import { chunk } from "lodash";
+import { DAYS_TILL_STALE, STEAM_API_PARALLEL_REQUESTS } from "../config";
+import throttledQueue from "throttled-queue";
+
+const throttle = throttledQueue(1, 1500);
+
+const infra = getInfra();
+const services = getServices();
+
+const currentDate = new Date();
+
+async function initSteamGameDatabase(baseGames: BaseGame[]) {
+  try {
+    const { steamService } = services;
+
+    const ids = baseGames.map((game) => game.steam_id);
+    const steamGames = await infra.sqliteDatbase.getSteamAppsByIds(ids);
+
+    const failedSteamGames = await infra.sqliteDatbase.getAllFailedSteamGames();
+
+    const mappedSteamGame: Map<number, SteamApp> = new Map();
+    for (const steamGame of steamGames) {
+      mappedSteamGame.set(steamGame.steam_appid, steamGame);
+    }
+
+    const mappedFailedSteamGame: Map<number, FailedSteamGame> = new Map();
+    for (const failedSteamGame of failedSteamGames) {
+      mappedFailedSteamGame.set(failedSteamGame.steam_id, failedSteamGame);
+    }
+
+    const filteredBaseGames = baseGames.filter((game) => {
+      return !checkIfAlreadyInit(
+        game.steam_id,
+        mappedSteamGame,
+        mappedFailedSteamGame
+      );
+    });
+    const chunkedGames = chunk(filteredBaseGames, STEAM_API_PARALLEL_REQUESTS);
+
+    let index = 0;
+    for (const batchGames of chunkedGames) {
+      const steamAppPromises = batchGames.map((game) => {
+        return infra.steamApi.getGame(game.steam_id);
+      });
+
+      const results = await throttle(() => {
+        return Promise.allSettled(steamAppPromises);
+      });
+
+      for (const singleResult of results) {
+        if (singleResult.status === "fulfilled" && singleResult.value) {
+          const steamApp = singleResult.value;
+          try {
+            await infra.sqliteDatbase.insertSteamApp(steamApp);
+          } catch (error) {
+            console.log(`Something went wrong on insert [${error}]`);
+          }
+        }
+        index++;
+        console.log(`Index [${index}] out of [${chunkedGames.length}]`);
+      }
+    }
+  } catch (error) {
+    console.log("caught error...........", error);
+  }
+}
+
+function checkIfAlreadyInit(
+  steamId: number,
+  mappedSteamGame: Map<number, SteamApp>,
+  mappedFailedGame: Map<number, FailedSteamGame>
+) {
+  if (mappedSteamGame.has(steamId)) {
+    const steamGame = mappedSteamGame.get(steamId)!;
+    const staleDate = addDays(new Date(steamGame.createdAt), DAYS_TILL_STALE);
+    if (isBefore(currentDate, staleDate)) {
+      return true;
+    }
+  }
+  if (mappedFailedGame.has(steamId)) {
+    const failedSteamGame = mappedFailedGame.get(steamId)!;
+    const staleDate = addDays(
+      new Date(failedSteamGame.createdDate),
+      DAYS_TILL_STALE
+    );
+    if (isBefore(currentDate, staleDate)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+async function run() {
+  await infra.sqliteDatbase.connectDb();
+  const allBaseGames = await infra.sqliteDatbase.getAllBaseGames();
+  const reversedAllBaseGames = allBaseGames.reverse();
+  initSteamGameDatabase(reversedAllBaseGames);
+}
+
+run();
---
+++ scripts/createIndieYearlyTagDistribution.ts
@@ -0,0 +1,99 @@
+import getInfra from "../infra/getInfrastructure";
+import { addYears, isAfter, isBefore } from "date-fns";
+import { SteamApp } from "../infra/steam-api";
+
+const infra = getInfra();
+
+const fromDate = new Date("2022-04-08");
+const toDate = addYears(fromDate, 1);
+
+const tableName = "indie_2022_4__2023_4";
+
+interface PreData {
+  tag: string;
+  gameCount: number;
+  recommendationCount: number;
+  averageUSDPrice: number;
+}
+
+function mapSingleGame(
+  data: { [tag: string]: PreData },
+  steamGame: SteamApp,
+  tags: string[]
+) {
+  for (const singleTag of tags) {
+    if (!data[singleTag]) {
+      data[singleTag] = {
+        tag: singleTag,
+        gameCount: 1,
+        recommendationCount: steamGame.totalRecommendation,
+        averageUSDPrice: steamGame.priceUSD,
+      };
+    }
+    const newGameCount = data[singleTag].gameCount + 1;
+    data[singleTag] = {
+      tag: singleTag,
+      gameCount: newGameCount,
+      recommendationCount:
+        data[singleTag].recommendationCount + steamGame.totalRecommendation,
+      averageUSDPrice:
+        (data[singleTag].averageUSDPrice * data[singleTag].gameCount +
+          steamGame.priceUSD) /
+        newGameCount,
+    };
+  }
+}
+
+async function mapData(
+  steamGames: SteamApp[],
+  mappedTags: { [steam_id: string]: string[] }
+) {
+  const data: { [tag: string]: PreData } = {};
+  for (const singleSteamGame of steamGames) {
+    if (!singleSteamGame.isReleased || !singleSteamGame.releaseDateMill) {
+      continue;
+    }
+    if (
+      isAfter(singleSteamGame.releaseDateMill, toDate) ||
+      isBefore(singleSteamGame.releaseDateMill, fromDate)
+    ) {
+      continue;
+    }
+    if (!mappedTags[singleSteamGame.steam_appid]) {
+      continue;
+    }
+
+    const tags = mappedTags[singleSteamGame.steam_appid];
+    mapSingleGame(data, singleSteamGame, tags);
+  }
+
+  const { sqliteDatbase } = infra;
+  for (const [_key, singleData] of Object.entries(data)) {
+    await sqliteDatbase.insertCustomRawData(tableName, singleData);
+  }
+}
+
+async function run() {
+  await infra.sqliteDatbase.connectDb();
+  await infra.sqliteDatbase.ensureTagDistributionTable(tableName);
+
+  const mappedTags: { [steam_id: string]: string[] } = {};
+  const tags = await infra.sqliteDatbase.getAllSteamTags();
+  for (const tag of tags) {
+    mappedTags[tag.steam_id] = tag.tag;
+  }
+
+  const steamGames = await infra.sqliteDatbase.getSteamGamesFromDateRange(
+    fromDate.getTime(),
+    toDate.getTime()
+  );
+  const indieSteamGames = steamGames.filter((steamGame) => {
+    if (!mappedTags[steamGame.steam_appid]) {
+      return false;
+    }
+    return mappedTags[steamGame.steam_appid].includes("Indie");
+  });
+  await mapData(indieSteamGames, mappedTags);
+}
+
+run();
